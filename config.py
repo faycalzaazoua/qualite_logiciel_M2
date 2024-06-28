f81acaa2da9f4d7638b2848f1e4d799024b719a5
faycalzaazoua <zaazoua.faycal1@gmail.com>
2024-06-28 23:05:32 +0200

    remove secret

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-GITHUB_TOKEN = "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"
-OPENAI_API_KEY = "sk-proj-cyy5VqdAHKfwYHOXi0f5T3BlbkFJPFtMx50WTAievpC5Jngx"  
 OWNER = "faycalzaazoua" 
 REPO = "qualite_logiciel"  
 PR_NUMBER = 1
